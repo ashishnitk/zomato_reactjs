6f0b2c8789dc7f2e43a067fc57b4ff7f0b6b7142
Ashish Prasad <aspras@microsoft.com>
2023-07-13 23:45:07 +0530

    udpaede

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import HomePage from './pages/home';
+
+const App = () => {
+  return <HomePage />;
+};
+
+export default App;
---
+++ src/components/common/footer/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="absolute-center max-width">Made with 🔥 by Ashish</div>
+  );
+};
+
+export default Footer;
